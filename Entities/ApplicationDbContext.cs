0fb475f335f1c5352d17566dc146334a633370cb
victorvega <victor.vega@protecso.com.pe>
2019-12-05 17:46:45 -0500

    ok

---
+++ Entities/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace jwtAuth.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            const string databaseName = "jwtAuth";
+            const string databaseUser = "root";
+            const string databasePass = "root";
+
+            return $"Server=localhost;" +
+                   $"database={databaseName};" +
+                   $"uid={databaseUser};" +
+                   $"pwd={databasePass};" +
+                   $"pooling=true;";
+        }
+    }
+}
